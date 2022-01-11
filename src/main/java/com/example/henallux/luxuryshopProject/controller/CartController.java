5cd911d182b10450639a3aceeb6a9868438cdd3d
POManil <49270963+POManil@users.noreply.github.com>
2022-01-11 19:41:42 +0100

    Update CartController.java

--- src/main/java/com/example/henallux/luxuryshopProject/controller/CartController.java
+++ src/main/java/com/example/henallux/luxuryshopProject/controller/CartController.java
@@ -12,17 +12,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Locale;
 
 @Controller
 @RequestMapping(value = "/cart")
 @SessionAttributes({Constants.CURRENT_CART})
 public class CartController {
+    private Boolean saveCurrCart = false;
 
+    private ProductDataAccess productDataAccess;
+    private OrderDataAccess orderDataAccess;
+    private OrderLineDataAccess orderLineDataAccess;
     private final PurchaseManager purchaseManager;
 
     @Autowired
-    public CartController(PurchaseManager purchaseManager){
+    public CartController(PurchaseManager purchaseManager, ProductDAO productDAO, OrderDAO orderDAO, OrderLineDAO orderLineDAO){
+        this.productDataAccess = productDAO;
+        this.orderDataAccess = orderDAO;
+        this.orderLineDataAccess = orderLineDAO;
         this.purchaseManager = purchaseManager;
     }
 
@@ -62,18 +71,53 @@ public class CartController {
         return "redirect:/cart";
     }
 
-    @RequestMapping(value = "/paymentSuccess", method = RequestMethod.POST)
-    public String paymentSuccess(){
-        return "ajax:paymentSuccess";
+@RequestMapping(value = "/paymentSuccess", method = RequestMethod.GET)
+    public String paymentSuccess(@ModelAttribute(value=Constants.CURRENT_CART) Cart cart){
+        setSaveCurrCart(false);
+        cart.getItems().clear();
+
+        return "redirect:/home";
     }
-    @RequestMapping(value = "/paymentFailed", method = RequestMethod.POST)
+    @RequestMapping(value = "/paymentFailed", method = RequestMethod.GET)
     public String paymentFailed(){
-        return "ajax:paymentFailed";
+        return "redirect:/cart";
+    }
+
+    @RequestMapping(value = "/saveOrder", method = RequestMethod.POST)
+    public String saveOrder(Authentication authentication, Model model, @ModelAttribute(value = "cartItem")CartItem cartItem, @ModelAttribute(value = Constants.CURRENT_CART)Cart cart){
+
+        Customer customer = (Customer) authentication.getPrincipal();
+        Date today = new Date();
+
+        Order order = new Order(today, customer);
+        order.setId(orderDataAccess.save(order).getId());
+
+        ArrayList<OrderLine> orderLines = new ArrayList<>();
+        for (CartItem item : cart.getItems().values()){
+            Product product = productDataAccess.findProductEntityByName(item.getLabel());
+            orderLines.add(new OrderLine(item.getQuantity(), item.getPrice(), product, order));
+        }
+
+        for(OrderLine orderLine : orderLines){
+            orderLine = orderLineDataAccess.save(orderLine);
+        }
+
+        setSaveCurrCart(true);
+        model.addAttribute("cartSaved", getSaveCurrCart());
+        return "redirect:/cart";
     }
 
 
     @ModelAttribute(Constants.CURRENT_CART)
     public Cart newCart (){
         return new Cart();
+    }
+
+    public void setSaveCurrCart(Boolean saveCurrCart) {
+        this.saveCurrCart = saveCurrCart;
+    }
+
+    public Boolean getSaveCurrCart() {
+        return saveCurrCart;
     }
 }
